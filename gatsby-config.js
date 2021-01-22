d99c6bc43f1c02800b845c3274643c881d52960f
Michael Steichen <michael.steichen@gmail.com>
2021-01-21 19:34:44 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMetadata: {
+    title: "personal-website",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "",
+      },
+    },
+  ],
+};
