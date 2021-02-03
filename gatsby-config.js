96f0bb36415cb25898af218fada9a0505f0aecae
Michael Steichen <michael.steichen@gmail.com>
2021-02-03 16:58:03 -0600

    adding ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,12 +33,22 @@ module.exports = {
         },
       },
     },
-    `gatsby-plugin-offline`
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-YGDQD5M1Y0"
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+        },
+      },
+    },
   ],
-    // {
-    //   resolve: "gatsby-plugin-google-analytics",
-    //   options: {
-    //     trackingId: "",
-    //   },
-    // },
 };
